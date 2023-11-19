9a752b988e4e022d92b977d12858ba9f402ba4a7
김재령 <wofud0321@naver.com>
2023-11-19 15:02:06 +0900

    docker-compose 설정

--- src/main/java/com/springbatch/project/springbatch/SpringBatchApplication.java
+++ src/main/java/com/springbatch/project/springbatch/SpringBatchApplication.java
@@ -1,13 +1,10 @@
 package com.springbatch.project.springbatch;
 
-import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @EnableBatchProcessing
 @SpringBootApplication
@@ -18,7 +15,16 @@ public class SpringBatchApplication {
 		BatchConfigurer제거 DefaultBatchConfigurer되었습니다.
 		더 이상 이 인터페이스와 기본 구현을 사용하여 동작을 사용자 정의해서는 안 됩니다.@EnableBatchProcessing
 		라고 쓰여져 있다.
+		https://velog.io/@cho876/Spring-Batch-job-%EC%83%9D%EC%84%B1 바뀐 방식 여기서 자세히
 	 */
+
+	/*private final JobBuilder jobBuilder;
+	private final StepBuilder stepBuilder;
+
+	public SpringBatchApplication(JobBuilder jobBuilder, StepBuilder stepBuilder) {
+		this.jobBuilder = jobBuilder;
+		this.stepBuilder = stepBuilder;
+	}*/
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBatchApplication.class, args);
