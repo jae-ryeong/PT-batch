7b4fb601f6723bb41916c06802eff66521d38f52
김재령 <wofud0321@naver.com>
2023-11-27 08:23:27 +0900

    카카오톡 메세지 관련 클래스생성

---
+++ src/main/java/com/springbatch/project/springbatch/config/KakaoTalkMessageConfig.java
@@ -0,0 +1,14 @@
+package com.springbatch.project.springbatch.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter @Setter
+@Component
+@ConfigurationProperties(prefix = "kakaotalk")
+public class KakaoTalkMessageConfig {
+    private String host;
+    private String token;
+}
